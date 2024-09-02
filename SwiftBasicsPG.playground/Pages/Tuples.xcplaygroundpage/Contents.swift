bac0a0539ff47bb1dee0fa7982b4b9fbe132ad2b
Anand Yadav <aanand.18jan@gmail.com>
2024-09-02 17:37:34 +0530

    Commit

--- SwiftBasicsPG.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
+++ SwiftBasicsPG.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
@@ -2,12 +2,4 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
-var names = ["Anand", "Ritesh", "Khushi", "Viren", "Sneha", "Ravi"]
-
-var arr = [12, 45, 84, 25, 45, 38, 62]
-
-for a in arr where a % 2 == 0 {
-    print(a)
-}
 //: [Next](@next)
